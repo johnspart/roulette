ddd036f557dd6ef98d1dd089488304319de4bab6
John <john.lopez>
2021-04-16 15:10:37 -0500

    first commit

---
+++ src/test/java/roulette/game/RouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package roulette.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
